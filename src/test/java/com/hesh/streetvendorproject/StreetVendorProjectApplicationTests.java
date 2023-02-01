246d0b11bb98dd4aa46bd547c79add0b8005cb8d
heshikaimesh <102600305+heshikaimesh@users.noreply.github.com>
2023-02-01 12:34:10 +0530

    Update StreetVendorProjectApplicationTests.java

--- src/test/java/com/hesh/streetvendorproject/StreetVendorProjectApplicationTests.java
+++ src/test/java/com/hesh/streetvendorproject/StreetVendorProjectApplicationTests.java
@@ -1,13 +1,1 @@
-package com.hesh.streetvendorproject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StreetVendorProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
